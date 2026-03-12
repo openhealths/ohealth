a6cd8b80e9d2ef28d0b8200dfa234f9f9fed2de4
Natalia <nataliakulyk123@gmail.com>
2026-03-12 09:29:11 +0200

    #25 fix

--- resources/views/livewire/dictionary/medication-program.blade.php
+++ resources/views/livewire/dictionary/medication-program.blade.php
@@ -79,7 +79,7 @@
                         <span x-text="dictionaries.FUNDING_SOURCE[selectedProgram.funding_source]"></span>
                     </p>
                     <p>
-                        <span class="font-semibold">{{ __('dictionaries.medication_programs.mr_blank_type') }}:</span>
+                        <span class="font-semibold">{{ __('dictionaries.mr_blank_type') }}:</span>
                         <span x-text="selectedProgram.mr_blank_type"></span>
                     </p>
                     <p>
